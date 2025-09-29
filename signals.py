a30b5f76dfd738641e6e506bbca16d3cf5d7b3b5
Rick vant Verlaat <R.P.D.Verlaat@student.tudelft.nl>
2025-09-29 17:13:37 +0200

    Added unit step function and docstring for sine wave

--- signals.py
+++ signals.py
@@ -6,6 +6,26 @@ Created on Sun Sep 21 13:05:53 2025
 """
 import numpy as np
 def generate_sine_wave(frequency, duration, sample_rate):
+    """
+    generates a sine wave
+
+    Parameters
+    ----------
+    frequency : .
+    duration : how long the sine wave continues for.
+    sample_rate : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    y : TYPE
+        DESCRIPTION.
+
+    """
     t = np.linspace(0, duration, sample_rate)
     y = np.sin(frequency*2*np.pi*t)
     return y
+
+def u(start, finish, delay, amplitude):
+    t = np.linspace(start, finish, 1000)
+    return np.where(t<0 - delay, 0, amplitude)
