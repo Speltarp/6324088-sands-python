408a3074bec9b9b195faa8ca203d86a93dfbb31c
Rick vant Verlaat <R.P.D.Verlaat@student.tudelft.nl>
2025-10-01 16:03:40 +0200

    Updated run file so that the duration works correctly

--- run.py
+++ run.py
@@ -7,14 +7,25 @@ Created on Sun Sep 21 13:16:48 2025
 import signals as sig
 import matplotlib.pyplot as plt
 
+#generate a sine wave
 print(sig.generate_sine_wave(5, 2, 100)[0:10])
 
+t, y = sig.generate_sine_wave(5, 2, 100)
 plt.figure(1)
-plt.plot(sig.generate_sine_wave(5, 2, 100))
-
-plt.figure(2)
-plt.plot(t, sig.u(-10, 10, -1, 2))
+plt.plot(t, y)
 plt.grid(True)
+plt.title("Sine wave")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+
+#generate unit step function
+t, y = sig.u(-10, 10, -2, 3, 1000)
+plt.figure(2)
+plt.plot(t, y)
+plt.grid(True)
+plt.title("Unit Step function")
+plt.xlabel("Time")
+plt.ylabel("Height")
 plt.show()
 
 
