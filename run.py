648bbffe314097f0e02a12c2881ae030c5ccec57
Rick vant Verlaat <R.P.D.Verlaat@student.tudelft.nl>
2025-10-02 20:14:38 +0200

    Added modified unit step function

--- run.py
+++ run.py
@@ -19,16 +19,31 @@ plt.xlabel("Time")
 plt.ylabel("Amplitude")
 
 #generate unit step function
-t, y = sig.u(-10, 10, -2, 3, 1000)
+t, y = sig.u(-10, 10, 1, 1000)
 plt.figure(2)
 plt.plot(t, y)
 plt.grid(True)
 plt.title("Unit Step function")
 plt.xlabel("Time")
 plt.ylabel("Height")
-plt.show()
 
+
+#generate a modified sine wave with higher amplitude and offset of 0.5, making it a cosine
 t, y = sig.modified_sine_wave(5, 2, 1000, 3, 0.5)
 plt.figure(3)
 plt.plot(t, y)
+plt.grid(True)
+plt.title("Modified sine wave")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+
+
+#generate a modified unit step function with a delay of 2
+t, y = sig.modified_u(-10, 10, -2, 3, 1000)
+plt.figure(4)
+plt.plot(t, y)
+plt.grid(True)
+plt.title("Modified unit Step function")
+plt.xlabel("Time")
+plt.ylabel("Height")
 plt.show()
--- signals.py
+++ signals.py
@@ -30,28 +30,16 @@ def generate_sine_wave(frequency, duration, sample_rate):
     y = np.sin(frequency*2*np.pi*t)
     return t, y
 
-def u(start, finish, delay, amplitude, sample_rate):
+def u(start, finish, amplitude, sample_rate):
     t = np.linspace(start, finish, sample_rate)
-    return t, np.where(t<0 - delay, 0, amplitude)
-#
-#def modifier(type_function, start, finish, delay, amplitude, sample_rate, frequency, duration, offset, extra_delay, extra_amplitude, extra_frequency):
- #   if type_function == "u":
-  #      t, y = u(start, finish, delay + extra_delay, amplitude + extra_amplitude, sample_rate)
-   #     
-    #if type_function == "sin":
-     #   t, y = generate_sine_wave(frequency + extra_frequency, duration, amplitude + extra_amplitude)
-    #return t, y
-
-
-#import matplotlib.pyplot as plt
-
-#t1, y1 = modifier("u", -10, 10, -2, 3, 1000, 0, 0, 0, -2, 2, 0)
-#plt.figure()
-#plt.show(t1, y1)
+    return t, np.where(t<0, 0, amplitude)
 
 def modified_sine_wave(frequency, duration, sample_rate, amplitude, Offset):
     t = np.linspace(0, duration, sample_rate)
     y = amplitude * np.sin(frequency*2*np.pi*t+Offset * np.pi)
     return t, y
     
+def modified_u(start, finish, delay, amplitude, sample_rate):
+    t = np.linspace(start, finish, sample_rate)
+    return t, np.where(t<0 - delay, 0, amplitude)
     
